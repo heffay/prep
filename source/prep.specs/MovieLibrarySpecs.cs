2f11f0a67046881a7215300bb4ec4ffc8409426f
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-05-14 17:25:07 -0600

    Updated

--- source/prep.specs/MovieLibrarySpecs.cs
+++ source/prep.specs/MovieLibrarySpecs.cs
@@ -202,7 +202,7 @@ namespace prep.specs
             It should_be_able_to_find_all_movies_published_by_pixar_or_disney = () =>
             {
                 var criteria = Where<Movie>.has_a(x => x.production_studio).equal_to_any(ProductionStudio.Pixar,ProductionStudio.Disney);
-                var results = sut.all_movies().all_items_matching(Movie.is_published_by_pixar_or_disney());
+                var results = sut.all_movies().all_items_matching(criteria);
 
                 results.ShouldContainOnly(a_bugs_life, pirates_of_the_carribean, cars);
             };
@@ -230,14 +230,16 @@ namespace prep.specs
 
             It should_be_able_to_find_all_kid_movies = () =>
             {
-                var results = sut.all_kid_movies();
+                var criteria = Where<Movie>.has_a(x => x.genre).equal_to_any(Genre.kids);
+                var results = sut.all_movies().all_items_matching(criteria);
 
                 results.ShouldContainOnly(a_bugs_life, shrek, cars);
             };
 
             It should_be_able_to_find_all_action_movies = () =>
             {
-                var results = sut.all_action_movies();
+                var criteria = Where<Movie>.has_a(x => x.genre).equal_to_any(Genre.action);
+                var results = sut.all_movies().all_items_matching(criteria);
 
                 results.ShouldContainOnly(indiana_jones_and_the_temple_of_doom, pirates_of_the_carribean);
             };
